4b93bcf5da0e256d3b66bc3cd6ebccefbf646704
Harmesh Kumar <kumarharmesh@hotmail.com>
2023-01-09 21:09:36 +0530

    D3

---
+++ Leap.cs
@@ -0,0 +1,19 @@
+using System;
+public class LeapClass
+{
+    public bool Compare(int x ,int y)
+    {
+        return x>y;
+    }
+    public string Name {get;set;}
+}
+public class P{
+    public int Mode{get;set;}
+}
+public class Murga
+{
+    public string DishType{get;set;}
+}
+
+public class Tent{}
+public class Mountain{}
