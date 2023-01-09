7fa9de851aedd8fd8e31f6875f1d001a20fb64c3
Harmesh Kumar <kumarharmesh@hotmail.com>
2023-01-09 21:02:18 +0530

    one

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTesting
+{
+    public class Class1
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public void Display()
+        {
+            Console.WriteLine($"{name}  {age}");
+        }
+    }
+    public class M
+    {
+        public string city{get;set;}
+        public string state{get;set;}
+        public int Pin {get;set;}
+        public string Country{get;set;}
+    }
+    
+}
